7399a2d610a4f3774249c26e24088123815e4276
Igor <68581590+igorcalabraro@users.noreply.github.com>
2025-02-19 12:53:45 -0300

    feat: get data from backend to mount ranking

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -1,9 +1,12 @@
+import { getRanking } from '@/http/api'
 import Image from 'next/image'
 import cooperSilver from '../../../assets/medal-cooper.svg'
 import goldMedal from '../../../assets/medal-gold.svg'
 import silverMedal from '../../../assets/medal-silver.svg'
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking()
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
@@ -11,53 +14,49 @@ export function Ranking() {
       </h2>
 
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> | Igor Calabraro
-          </span>
+        {ranking.map((item, index) => {
+          const rankingPosition = index + 1
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+          return (
+            <div
+              key={item.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}º</span> |{' '}
+                {item.name}
+              </span>
 
-          <Image
-            src={goldMedal}
-            alt="medalha de ouro"
-            className="absolute top-0 right-8"
-          />
-        </div>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {item.score}
+              </span>
 
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> | Igor Calabraro
-          </span>
+              {rankingPosition === 1 && (
+                <Image
+                  src={goldMedal}
+                  alt="medalha de ouro"
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1029
-          </span>
+              {rankingPosition === 2 && (
+                <Image
+                  src={silverMedal}
+                  alt="medalha de prata"
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-          <Image
-            src={silverMedal}
-            alt="medalha de prata"
-            className="absolute top-0 right-8"
-          />
-        </div>
-
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> | Igor Calabraro
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1028
-          </span>
-
-          <Image
-            src={cooperSilver}
-            alt="medalha de bronze"
-            className="absolute top-0 right-8"
-          />
-        </div>
+              {rankingPosition === 3 && (
+                <Image
+                  src={cooperSilver}
+                  alt="medalha de bronze"
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
