594b77772786660f874c3ea5c513226aa4935305
Igor <68581590+igorcalabraro@users.noreply.github.com>
2025-02-18 22:50:17 -0300

    feat: logo be an anchor to home page

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -10,7 +10,9 @@ export default function InvitePage() {
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
-        <Image src={logo} alt="devstage" width={108.5} height={30} />
+        <a href="/">
+          <Image src={logo} alt="devstage" width={108.5} height={30} />
+        </a>
 
         <div className="space-y-2">
           <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
