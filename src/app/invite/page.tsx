f8ce7837b01939f5b211c9b36f68212361213081
Igor <68581590+igorcalabraro@users.noreply.github.com>
2025-02-18 22:14:48 -0300

    feat: consuming invite components instead of directly code

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,11 +1,8 @@
-import { IconButton } from '@/components/icon-button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import Image from 'next/image'
 import logo from '../../assets/logo.svg'
-import cooperSilver from '../../assets/medal-cooper.svg'
-import goldMedal from '../../assets/medal-gold.svg'
-import silverMedal from '../../assets/medal-silver.svg'
+import { InviteLinkInput } from './invite-link-input'
+import { Ranking } from './ranking'
+import { Stats } from './stats'
 
 export default function InvitePage() {
   return (
@@ -34,111 +31,12 @@ export default function InvitePage() {
             </p>
           </div>
 
-          <InputRoot>
-            <InputIcon>
-              <Link className="size-5" />
-            </InputIcon>
-
-            <InputField
-              readOnly
-              defaultValue={'http://localhost:3000/invite/07140502011910'}
-            />
-
-            <IconButton className="-mr-2">
-              <Copy className="size-5" />
-            </IconButton>
-          </InputRoot>
-
-          <div className="grid gap-3 md:grid-cols-3">
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Acessos ao link
-              </span>
-
-              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Inscrições feitas
-              </span>
-
-              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                3º
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Posição no ranking
-              </span>
-
-              <Medal className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-          </div>
+          <InviteLinkInput />
+          <Stats />
         </div>
       </div>
 
-      <div className="w-full max-w-[440px] space-y-5">
-        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-          Ranking de indicações
-        </h2>
-
-        <div className="space-y-4">
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1º</span> | Diego Fernandes
-            </span>
-
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-
-            <Image
-              src={goldMedal}
-              alt="medalha de ouro"
-              className="absolute top-0 right-8"
-            />
-          </div>
-
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">2º</span> | Diego Fernandes
-            </span>
-
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1029
-            </span>
-
-            <Image
-              src={silverMedal}
-              alt="medalha de prata"
-              className="absolute top-0 right-8"
-            />
-          </div>
-
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">3º</span> | Diego Fernandes
-            </span>
-
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1028
-            </span>
-
-            <Image
-              src={cooperSilver}
-              alt="medalha de bronze"
-              className="absolute top-0 right-8"
-            />
-          </div>
-        </div>
-      </div>
+      <Ranking />
     </div>
   )
 }
