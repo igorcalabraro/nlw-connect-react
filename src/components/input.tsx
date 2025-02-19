1b8272017b1f4016781a58d6d831337ce8e547f5
Igor <68581590+igorcalabraro@users.noreply.github.com>
2025-02-18 21:48:42 -0300

    feat: merge classNames on components to prevent errors

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,14 +1,22 @@
 import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface InputRootProps extends ComponentProps<'div'> {
   error?: boolean
 }
 
-export function InputRoot({ error = false, ...props }: InputRootProps) {
+export function InputRoot({
+  error = false,
+  className,
+  ...props
+}: InputRootProps) {
   return (
     <div
       data-error={error}
-      className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger"
+      className={twMerge(
+        'group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger',
+        className
+      )}
       {...props}
     />
   )
@@ -16,10 +24,13 @@ export function InputRoot({ error = false, ...props }: InputRootProps) {
 
 interface InputIconProps extends ComponentProps<'span'> {}
 
-export function InputIcon(props: InputIconProps) {
+export function InputIcon({ className, ...props }: InputIconProps) {
   return (
     <span
-      className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
+      className={twMerge(
+        'text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger',
+        className
+      )}
       {...props}
     />
   )
@@ -27,6 +38,11 @@ export function InputIcon(props: InputIconProps) {
 
 interface InputFieldProps extends ComponentProps<'input'> {}
 
-export function InputField(props: InputFieldProps) {
-  return <input className="flex-1 outline-0 placeholder-gray-400" {...props} />
+export function InputField({ className, ...props }: InputFieldProps) {
+  return (
+    <input
+      className={twMerge('flex-1 outline-0 placeholder-gray-400', className)}
+      {...props}
+    />
+  )
 }
